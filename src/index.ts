75770cd6294bba2944815d7b3b99db3650582b56
maruf <marufsharifi97103@gmail.com>
2021-10-24 10:18:49 +0430

    Created login route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { router } from "./routes/loginRoute";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
-            <div>
-               <h1>Hi There!</h1>
-            </div>
-        `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
