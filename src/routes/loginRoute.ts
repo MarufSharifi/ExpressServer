64f4257e6b51db5e27279d0e27a250347097e3a9
maruf <marufsharifi97103@gmail.com>
2021-10-25 10:15:47 +0430

    overrite the body property of Request Type

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express";
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -18,7 +22,7 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
   res.send(`${email} ${password}`);
 });
