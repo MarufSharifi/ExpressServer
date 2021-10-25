fa2af2ea165d26c16829871b7e0fbc24e211707b
maruf <marufsharifi97103@gmail.com>
2021-10-25 10:21:19 +0430

    Implemented protected route

--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,7 +1,17 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send("not permitted");
 }
 
 const router = Router();
@@ -49,6 +59,10 @@ router.get("/", (req: Request, res: Response) => {
 router.get("/logout", (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected route Logged In user");
 });
 
 export { router };
