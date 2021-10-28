68abb0bb202b2b0f752b11515429ad0846fd7bbb
maruf <marufsharifi97103@gmail.com>
2021-10-28 10:09:53 +0430

    Created methods and metadatakey enums

--- src/controller/decorators/controller.ts
+++ src/controller/decorators/controller.ts
@@ -1,15 +1,26 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
+import { Methods } from "./methods";
+import { MetadataKeys } from "./MetadataKeys";
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
     const router = AppRouter.getInstance();
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata("path", target.prototype, key);
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      );
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
--- src/controller/decorators/routes.ts
+++ src/controller/decorators/routes.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
+import { Methods } from "./methods";
+import { MetadataKeys } from "./MetadataKeys";
+
 function routeBuilder(method: string) {
   return function (path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     };
   };
 }
 
-export const get = routeBuilder("get");
-export const post = routeBuilder("post");
-export const patch = routeBuilder("patch");
-export const del = routeBuilder("delete");
-export const put = routeBuilder("put");
+export const get = routeBuilder(Methods.get);
+export const post = routeBuilder(Methods.post);
+export const patch = routeBuilder(Methods.patch);
+export const del = routeBuilder(Methods.del);
+export const put = routeBuilder(Methods.put);
