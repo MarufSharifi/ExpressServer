da1b71eae2fa669dcd539b247a4d8f2ae7c8ecfa
maruf <marufsharifi97103@gmail.com>
2021-10-28 09:14:00 +0430

    refactored the login route to use decorators

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { router } from "./routes/loginRoute";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
-
+import { router as ControllerRouter } from "./controller/decorators/controller";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["lastKey"] }));
 app.use(router);
+app.use(ControllerRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -16,22 +16,6 @@ function requireAuth(req: Request, res: Response, next: NextFunction) {
 
 const router = Router();
 
-router.get("/login", (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-        <div>
-            <label>Email</label>
-            <input name="email"/>
-        </div>
-        <div>
-            <label>Password</label>
-            <input name="password" type="password"/>
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-  `);
-});
-
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
   if (email && password && email == "hi@hi.com" && password === "pass") {
